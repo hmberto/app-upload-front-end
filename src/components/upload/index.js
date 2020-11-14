45cb1348ec4d199e98e8b7d1f7756a98303ab066
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-14 19:32:00 -0300

    validation messages

--- src/components/upload/index.js
+++ src/components/upload/index.js
@@ -3,16 +3,27 @@ import Dropzone from 'react-dropzone'
 import { DropContainer, UploadMessage } from './styles'
 
 export default class Upload extends Component {
+  renderDragMessage = (isDragActive, isDragReject) => {
+    if(!isDragActive){
+      return <UploadMessage>arraste e solte arquivos aqui</UploadMessage>
+    }
+    if(isDragReject){
+      return <UploadMessage type="error">arquivo não suportado</UploadMessage>
+    }
+    return <UploadMessage type="success">solte os arquivos</UploadMessage>
+  }
+
   render() {
     return (
       <Dropzone accept="image/*" onDropAccepted={() => {}}>
-        { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
           <DropContainer
             {...getRootProps()}
             isDragActive={isDragActive}
             isDragReject={isDragReject}
           >
             <input {...getInputProps()} />
+            {this.renderDragMessage(isDragActive, isDragReject)}
           </DropContainer>
         )}
       </Dropzone>
