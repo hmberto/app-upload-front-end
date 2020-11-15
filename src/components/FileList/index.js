18101ccb161e8cc2c63120e51786cbb626c360e0
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-15 01:10:35 -0300

    FileList autoincrement added

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -4,36 +4,49 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="http://localhost:3000/files/81b0fb8f78cdd63f298621cd52861b91-fantasy-monster-cthulhu-uhdpaper.com-4K-158.jpg" />
+    {files.map(uploadedFile => (
+      <li key={uploadedFile.id}>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize}{" "}
+              {!!uploadedFile.url &&(
+                <button onClick={() => onDelete(uploadedFile.id)}>Excluir</button>
+              )}
+            </span>
+          </div>
+        </FileInfo>
         <div>
-          <strong>profile.png</strong>
-          <span>64kb <button onClick={() => {}}>Excluir</button></span>
+          {!uploadedFile.uploaded && !uploadedFile.error && (
+            <CircularProgressbar
+              styles={{
+                root: { width: 24 },
+                path: { stroke: '#7159c1' }
+              }}
+              strokeWidth={10}
+              value={uploadedFile.progress}
+            />
+          )}
+          
+          {uploadedFile.url && (
+            <a
+              href={uploadedFile.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+            </a>
+          )}
+
+          {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+          {uploadedFile.error && <MdError size={24} color="#e57878" />}
         </div>
-      </FileInfo>
-      <div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: '#7159c1' }
-          }}
-          strokeWidth={10}
-          value={60}
-        />
-        <a
-          href="http://localhost:3000/files/81b0fb8f78cdd63f298621cd52861b91-fantasy-monster-cthulhu-uhdpaper.com-4K-158.jpg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a>
-        <MdCheckCircle size={24} color="#78e5d5" />
-        <MdError size={24} color="#e57878" />
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
 )
 
