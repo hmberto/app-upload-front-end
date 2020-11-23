324000eec4e9f31e20ba51f7a161cc17a3313a34
Humberto Dantas <humbertoodantas@gmail.com>
2020-11-22 23:26:34 -0300

    tests/upload-test.js updated

--- tests/upload-test.js
+++ tests/upload-test.js
@@ -1,14 +1,9 @@
 import 'cypress-file-upload'
 
 describe('Upload Image', () => {
-  it('Upload Image too large', () => {
-    cy.visit('/')
-    const yourFixturePath = './../../img/img02.png'
-    cy.get('.sc-hKgILt').attachFile(yourFixturePath, { subjectType: 'drag-n-drop' });
-  })
-
   it('Upload Image', () => {
+    cy.visit('/')
     const yourFixturePath = './../../img/img01.png'
-    cy.get('.sc-hKgILt').attachFile(yourFixturePath, { subjectType: 'drag-n-drop' });
+    cy.get('.sc-hKgILt').attachFile(yourFixturePath, { subjectType: 'drag-n-drop' })
   })
 })
